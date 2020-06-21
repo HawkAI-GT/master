ec665720939e5522fe0315a048c64ef785939f28
Fernando González <rmfernando@outlook.com>
2020-06-21 13:58:02 -0600

    UserCardActions component added

---
+++ Files Bitbucket/user/src/components/UserCardActions.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-card-actions>
+    <v-btn text>Cancel</v-btn>
+    <v-spacer></v-spacer>
+    <v-btn color="primary" text @click="submit">Save</v-btn>
+  </v-card-actions>
+</template>
+
+<script>
+export default {
+  name: "UserCardActions"
+};
+</script>

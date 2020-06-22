5b2294a150aac28c2974a78dd73d51ae5bb1a43f
José Alejandro Guzmán Zamora <joseguzmanzamora@gmail.com>
2020-06-22 22:29:32 +0000

    Revert "Signup (pull request #4)"
    This reverts pull request #4.
    
    > * UserProfile view added
    > * UserInfo component added
    > * UserPic component added
    > * UserCardActions component added
    > * inicio formulario sign up
    > * Continuing on the signup form
    > * Sign up put font
    > * Edit user and user updates added
    > * Support section and Edit button with route added.
    > * User metadata component created and User components updates.
    > * Additional information page
    > * Finished the additional info
    > * additional test
    > * Test sign up
    > * no build only test
    > * back at it with the build
    > * Test file created and Font added. Other files also changed names.
    > * User views updated, rename some components.
    > * User Components name update and Test file v1 created
    > * Test file update
    > * conflicts app.vue
    > * Changed snapshot folder
    > 
    > ‌

--- Files Bitbucket/user/src/views/SignUp.vue
+++ Files Bitbucket/user/src/views/SignUp.vue
@@ -1,32 +1,5 @@
 <template>
   <div class="signup">
-    <v-row class="my-2">
-      <v-col cols="4" justify="end">
-        <v-img src="../assets/Isotipo de HawkAI.png" alt=""
-        contain   
-        height="50px" 
-        />
-      </v-col>
-      <v-col cols="8">
-        <h1 align="left">
-          Sign Up
-        </h1>
-      </v-col>
-    </v-row>
-    <SignUpForm />
+    <h1>This is a sign up page!</h1>
   </div>
 </template>
-
-<script>
-import SignUpForm from '../components/SignUpForm'
-
-export default {
-  name:"signup",
-  components:{
-    SignUpForm
-  }
-}
-</script>
-
-<style scoped>
-</style>

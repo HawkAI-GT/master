690c098eefe5fd6797f1eadfa5edddb75b13392a
Fernando González <rmfernando@outlook.com>
2020-06-21 18:35:21 -0600

    Edit user and user updates added

---
+++ Files Bitbucket/user/src/views/EditProfile.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-card class="mx-auto" max-width="434" tile>
+    <UserPic v-bind:user="user" />
+    <v-card-subtitle class="pb-0">Account Information</v-card-subtitle>
+    <UserInfo v-bind:user="user" />
+    <UserCardActions />
+  </v-card>
+</template>
+
+<script>
+import UserInfo from "../components/UserMetadata.vue";
+import UserPic from "../components/UserInfo.vue";
+import UserCardActions from "../components/UserCardActions.vue";
+
+export default {
+  components: {
+    UserInfo,
+    UserPic,
+    UserCardActions
+  },
+  data() {
+    return {
+      user: {
+        name: "Marcus Obrien",
+        job: "Network Engineer",
+        age: "26",
+        email: "example@aja.com",
+        dir: "My house",
+        phone: "+502 3404-4860"
+      }
+    };
+  }
+};
+</script>
---
+++ Files Bitbucket/user/src/views/User.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-card class="mx-auto" max-width="434" tile>
+    <UserPic v-bind:user="user" />
+    <v-card-subtitle class="pb-0">Account Information</v-card-subtitle>
+    <UserInfo v-bind:user="user" />
+    <v-divider class="mx-4"></v-divider>
+    <AccountInfo />
+    <v-divider class="mx-4"></v-divider>
+    <Support />
+    <ButtonEdit />
+  </v-card>
+</template>
+
+<script>
+import UserPic from "../components/UserInfo.vue";
+import ButtonEdit from "../components/ButtonEdit";
+import AccountInfo from "../components/AccountInfo";
+import Support from "../components/Support";
+
+export default {
+  name: "User",
+
+  components: {
+    UserPic,
+    ButtonEdit,
+    AccountInfo,
+    Support
+  },
+  data() {
+    return {
+      user: {
+        name: "Marcus Obrien",
+        job: "Network Engineer",
+        age: "26",
+        email: "example@aja.com",
+        dir: "My house",
+        phone: "+502 3404-4860"
+      }
+    };
+  }
+};
+</script>

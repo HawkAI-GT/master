fcd2cce9cc17f69e2ab273807a07a0f5f06d8df2
Fernando González <rmfernando@outlook.com>
2020-06-21 18:37:29 -0600

    Support section and Edit button with route added.

---
+++ Files Bitbucket/user/src/components/ButtonEdit.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-card-actions>
+    <v-btn outlined color="red" depressed text>Exit</v-btn>
+    <v-spacer></v-spacer>
+    <v-btn outlined color="primary" to="/userprofile" text>Edit</v-btn>
+  </v-card-actions>
+</template>
+
+<script>
+export default {
+  name: "Account Edit"
+};
+</script>

dfb0f360348229d78d681d58d59b93b418eee35a
Fernando González <rmfernando@outlook.com>
2020-06-22 12:33:35 -0600

    Test file update

--- Files Bitbucket/user/tests/unit/test.spec.js
+++ Files Bitbucket/user/tests/unit/test.spec.js
@@ -4,14 +4,11 @@ import EditProfile from '@/views/EditProfile.vue'
 
 describe('Component', () => {
     // Now mount the component and you have the wrapper
-    const wrapper = mount(User)
+    // const wrapper = mount(User)
 
-    it('renders the correct markup', () => {
-        expect(wrapper.html()).toContain('<div class="v-card__subtitle pb-0">Account Information</div>')
-    }),
 
-        test('EditProfile renders correctly', () => {
-            const wrapper = mount(EditProfile)
-            expect(wrapper.element).toMatchSnapshot()
-        })
+    test('EditProfile renders correctly', () => {
+        const wrapper = mount(EditProfile)
+        expect(wrapper.element).toMatchSnapshot()
+    })
 })

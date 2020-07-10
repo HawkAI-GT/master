d79b9e225f1e910c2d17d0a508607f9bb4f597ef
Fernando González <rmfernando@outlook.com>
2020-07-09 20:30:43 -0600

    Gradient background added.

--- FilesBitbucket/user/src/components/LoginBackground.vue
+++ FilesBitbucket/user/src/components/LoginBackground.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="imageContainer">
-    <v-img src="../assets/vertical_background.png" height="100vh" max-height="100vh"></v-img>
+  <div id="landing-page">
+    <v-row align="center" justify="center">
+      <v-img src="../assets/logohawkai.png" id="img" height="220px" width="220px" contain></v-img>
+    </v-row>
   </div>
 </template>
 
@@ -13,5 +15,30 @@ export default {
 <style scoped>
 .imageContainer {
   position: relative;
+}
+
+#landing-page {
+  background-image: linear-gradient(
+    90deg,
+    rgba(2, 0, 36, 1) 0%,
+    rgba(59, 59, 177, 1) 50%,
+    rgba(0, 212, 255, 1) 150%
+  );
+
+  height: 100vh;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  position: relative;
+}
+
+#img {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: 500px;
+  height: 500px;
+  margin-top: -290px; /* Half the height */
+  margin-left: -115px; /* Half the width */
 }
 </style>

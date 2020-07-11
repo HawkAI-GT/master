d2515c4bb96c99cec2e22d2944d4eee1188f6668
Victor Farfan <vafm98@hotmail.com>
2020-07-10 19:37:56 -0600

    removed test changes

--- FilesBitbucket/user/src/views/Login.vue
+++ FilesBitbucket/user/src/views/Login.vue
@@ -7,7 +7,7 @@
         <v-row>
           <v-col>
             <v-text-field
-              label="Username HOLA"
+              label="Username"
               v-model="username"
               name="username"
               single-line

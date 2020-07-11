f58940e85cfc8a477a02dfbbff0469a8edf40100
Victor Farfan <vafm98@hotmail.com>
2020-07-10 19:04:40 -0600

    Update Feed.vue

--- FilesBitbucket/user/src/views/Feed.vue
+++ FilesBitbucket/user/src/views/Feed.vue
@@ -1,15 +1,8 @@
 <template>
   <div>
-    <TopNav></TopNav>
-<<<<<<< HEAD
-    <FeedPost style="margin-bottom: 8vh;"></FeedPost>
-    <BottomNav />
-=======
-    <FeedPost></FeedPost>
-    <FeedPost></FeedPost>
+    <TopNav />
     <FeedPost style="margin-bottom: 8vh;"></FeedPost>
     <BottomNav></BottomNav>
->>>>>>> 7d68900e53ef2b358adefe9f783b2e9e8ac1d9fa
   </div>
 </template>
 <script>
